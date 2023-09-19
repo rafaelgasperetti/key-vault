d45f2dedd4ad422faa7d48e1ec1743202c70e952
Rafael Gasperetti <rafaelgasperetti@outlook.com>
2023-09-18 23:19:16 -0300

    Fix for controller working

--- key-vault/key-vault/Controllers/SecretController.cs
+++ key-vault/key-vault/Controllers/SecretController.cs
@@ -1,28 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace key_vault.Controllers
 {
+    [AllowAnonymous]
     [ApiController]
     [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/secret")]
     public class SecretController : ControllerBase
     {
-        [HttpGet]
-        [ValidateAntiForgeryToken]
+        [HttpGet("get/{key}")]
         public string Get(string key)
         {
             return key;
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        [HttpPost("create")]
+        //[ValidateAntiForgeryToken]
         public void Create(string key, string value)
         {
             
         }
 
-        [HttpDelete]
-        [ValidateAntiForgeryToken]
+        [HttpDelete("delete/{key}")]
+        //[ValidateAntiForgeryToken]
         public void Delete(string key)
         {
             
--- key-vault/key-vault/Program.cs
+++ key-vault/key-vault/Program.cs
@@ -1,34 +1,7 @@
 using key_vault.Models;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-
-/*var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();*/
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -69,3 +42,4 @@ app.UseEndpoints(endpoints =>
 });
 
 app.UseResponseCompression();
+app.Run();
